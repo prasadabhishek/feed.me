c43042147378b26564bccaf78e4b6885e998bba9
prasadabhishek <abhishek.a.prasad@gmail.com>
2014-07-14 15:13:23 -0400

    removed unnecessary libraries
    jsoup
    rss parser
    feed4j

--- src/com/feed/me/RemoteFetchService.java
+++ src/com/feed/me/RemoteFetchService.java
@@ -4,8 +4,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import nl.matshofman.saxrssreader.RssItem;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
